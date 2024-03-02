97665d7c9c9c47d1a7f0500c99e2aa8038605c4b
Andi Alifsyah <andyalyfsyah4@gmail.com>
2024-03-02 10:54:52 +0700

    isolate using env

--- account-service/database/model.py
+++ account-service/database/model.py
@@ -4,14 +4,19 @@ from sqlalchemy.engine import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '../.env'))
+
 # mendapatkan url untuk koneksi ke database postgresql
 uri = URL.create(
     drivername="postgresql",
-    username="root",
-    password="root",
-    host="localhost",
-    port="5432",
-    database="account-isi",
+    username=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    database=os.getenv("DB_NAME"),
 )
 
 # membuat engine untuk koneksi ke database
